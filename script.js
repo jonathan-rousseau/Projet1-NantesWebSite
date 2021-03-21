f73737902d81fb51473a36ec54d8f0444cd89546
Paul Bouhours <bouhours.paul@gmail.com>
2021-03-21 17:33:09 +0100

    création du message d'alerte de l'envoi du message dans la page Contact

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const navUl = document.getElementById('nav-ul');
 button.addEventListener ('click', function(event) {
     navUl.classList.toggle('show');
 })
+
+// page Contact
+
+const nom = document.getElementById("Nom");
+const email = document.getElementById("Email");
+const bouton = document.getElementById("btn-envoi");
+
+bouton.addEventListener('click', function(){
+    alert(`Merci pour votre Message ${nom.value} ! Nous vous répondrons sur votre messagerie ${email.value} . `)
+})
