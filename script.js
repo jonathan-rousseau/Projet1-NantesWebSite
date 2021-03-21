e20155968e19362c5e02163d3dbe51c1b4d1e918
Maxence <raingeonneau.m@gmail.com>
2021-03-21 19:24:31 +0100

    modif menu

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const button = document.getElementById('burgerbtn');
 const navUl = document.getElementById('nav-ul');
+let menuIcon = document.getElementById('menuicon');
 
 button.addEventListener ('click', function(event) {
     navUl.classList.toggle('show');
-})
+    //menuIcon.className = "fas fa-times";
+
+        if (menuIcon.className === "fas fa-bars") {
+            menuIcon.className = "fas fa-times";
+        }
+        else {
+            menuIcon.className = "fas fa-bars";
+        }
+}
+
+);
